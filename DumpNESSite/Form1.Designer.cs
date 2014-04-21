2c2defd339b5f080d28e78d4be975943d651fce0
Tom Kidd <tomkidd@gmail.com>
2014-04-21 18:04:15 -0500

    now with more source code

---
+++ DumpNESSite/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace DumpNESSite
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLinks = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtLinks
+            // 
+            this.txtLinks.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLinks.Location = new System.Drawing.Point(12, 12);
+            this.txtLinks.MaxLength = 2147483647;
+            this.txtLinks.Multiline = true;
+            this.txtLinks.Name = "txtLinks";
+            this.txtLinks.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLinks.Size = new System.Drawing.Size(804, 272);
+            this.txtLinks.TabIndex = 0;
+            this.txtLinks.WordWrap = false;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOutput.Location = new System.Drawing.Point(12, 319);
+            this.txtOutput.MaxLength = 2147483647;
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(804, 272);
+            this.txtOutput.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 290);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(828, 603);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtLinks);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLinks;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ DumpNESSite/Form1.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace DumpNESSite
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StringBuilder sbOutput = new StringBuilder();
+
+            string tempImage = Path.Combine(System.Environment.CurrentDirectory, "temp.jpg");
+
+            Microsoft.Office.Interop.Word.Application oWord;
+            Microsoft.Office.Interop.Word.Document oDoc;
+
+            object m = Type.Missing;
+
+            oWord = new Microsoft.Office.Interop.Word.Application();
+            oWord.Visible = true;
+
+            if (File.Exists("test.docx"))
+                File.Delete("test.docx");
+
+            oDoc = oWord.Documents.Add(ref m, ref m, ref m, ref m);
+
+            foreach (string url in txtLinks.Lines)
+            {
+                if (!string.IsNullOrWhiteSpace(url))
+                {
+                    HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+                    doc.OptionFixNestedTags = true;
+                    doc.LoadHtml(new System.Net.WebClient().DownloadString(url));
+                    string title = doc.DocumentNode.SelectSingleNode("/html/body/div[3]/div[2]/div[2]/div[2]/div[2]/div[2]/div[2]/div/div[4]/div/div/div/div/div/div/div/div/div/h3").InnerText;
+
+                    if (title.Split('-').Length == 2)
+                    {
+                        title = title.Split('-')[1].Trim();
+                    }
+
+                    HtmlAgilityPack.HtmlNode contentNode = doc.DocumentNode.SelectSingleNode("/html/body/div[3]/div[2]/div[2]/div[2]/div[2]/div[2]/div[2]/div/div[4]/div/div/div/div/div/div/div/div/div/div[2]");
+
+                    IEnumerable<HtmlAgilityPack.HtmlNode> imgNodes = contentNode.Descendants("img");
+
+                    Dictionary<string, HtmlAgilityPack.HtmlNode> changedNodes = new Dictionary<string, HtmlAgilityPack.HtmlNode>();
+
+                    foreach (HtmlAgilityPack.HtmlNode imgNode in imgNodes)
+                    {
+                        string src = imgNode.Attributes["src"].Value + ";" + imgNode.Attributes["width"].Value + ";" + imgNode.Attributes["height"].Value;
+                        changedNodes.Add(src, imgNode);
+                    }
+
+                    foreach (KeyValuePair<string, HtmlAgilityPack.HtmlNode> kvp in changedNodes)
+                    {
+                        HtmlAgilityPack.HtmlNode newNode = HtmlAgilityPack.HtmlNode.CreateNode("<p>" + kvp.Key + "</p>");
+                        kvp.Value.ParentNode.ReplaceChild(newNode, kvp.Value);
+                    }
+
+                    var scriptNodes = contentNode.Descendants("script").ToList();
+
+                    foreach (var scriptNode in scriptNodes)
+                        scriptNode.Remove();
+
+                    string content = contentNode.InnerText;
+
+                    string outputcontent = HtmlToText.ConvertHtml(content).Replace("\n", Environment.NewLine);
+
+                    outputcontent = outputcontent.Replace(Environment.NewLine + Environment.NewLine + Environment.NewLine, Environment.NewLine);
+
+                    //outputcontent = string.Join(
+                    //             Environment.NewLine,
+                    //             outputcontent.Split(new[] { Environment.NewLine }, StringSplitOptions.None).Select(s => s.Trim()));
+
+                    sbOutput.AppendLine(title);
+                    //sbOutput.Append(outputcontent);
+
+                    //outputcontent = Regex.Replace(outputcontent, @"^\s+$[\r\n]*", "", RegexOptions.Multiline);
+                    //outputcontent = Regex.Replace(outputcontent, @"$[\r\n\r\n\r\n]*", "", RegexOptions.Multiline);
+
+                    string[] outputsplit = outputcontent.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+
+                    //txtOutput.Text += outputcontent.Trim();
+
+                    Microsoft.Office.Interop.Word.Paragraph oPara1;
+                    oPara1 = oDoc.Content.Paragraphs.Add(ref m);
+                    oPara1.Range.Text = title;
+                    oPara1.Range.Font.Bold = 1;
+                    oPara1.Range.Font.Size = 24;
+                    //oPara1.Format.SpaceAfter = 24;
+                    oPara1.Range.InsertParagraphAfter();
+
+                    foreach (string outputline in outputsplit)
+                    {
+                        if (!string.IsNullOrWhiteSpace(outputline.Trim()))
+                        {
+                            Microsoft.Office.Interop.Word.Paragraph oPara2;
+                            oPara2 = oDoc.Content.Paragraphs.Add(ref m);
+
+                            if (outputline.StartsWith("http"))
+                            {
+                                byte[] tempImageData;
+
+                                using (System.Net.WebClient wc = new System.Net.WebClient())
+                                {
+                                    if (File.Exists(tempImage)) File.Delete(tempImage);
+                                    tempImageData = wc.DownloadData(outputline.Split(';')[0]);
+                                }
+
+                                Image tempImageOriginal;
+
+                                using (var ms = new MemoryStream(tempImageData))
+                                {
+                                    tempImageOriginal = Image.FromStream(ms);
+                                }
+
+                                Image tempImageResized = (Image)(new Bitmap(tempImageOriginal, new Size(int.Parse(outputline.Split(';')[1]), int.Parse(outputline.Split(';')[2]))));
+                                tempImageResized.Save(tempImage);
+
+                                oPara2.Alignment = Microsoft.Office.Interop.Word.WdParagraphAlignment.wdAlignParagraphCenter;
+
+                                oPara2.Range.InlineShapes.AddPicture(tempImage, false);
+                            }
+                            else
+                            {
+                                oPara2.Range.Font.Size = 10;
+
+                                string codePoint = "00A0";
+
+                                int code = int.Parse(codePoint, System.Globalization.NumberStyles.HexNumber);
+                                string unicodeString = char.ConvertFromUtf32(code).ToString();
+
+                                oPara2.Range.Text = outputline.Trim();
+
+                                if (outputline.StartsWith(unicodeString))
+                                {
+                                    oPara2.Range.Font.Italic = 1;
+                                    oPara2.Range.Font.Size = 8;
+                                    oPara2.Alignment = Microsoft.Office.Interop.Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                                }
+                                else
+                                {
+                                    oPara2.Range.Font.Italic = 0;
+                                    oPara2.Alignment = Microsoft.Office.Interop.Word.WdParagraphAlignment.wdAlignParagraphLeft;
+                                }
+
+                            }
+
+                            oPara2.Range.Font.Bold = 0;
+                            oPara2.Format.SpaceAfter = 10;
+                            oPara2.Range.InsertParagraphAfter();
+                        }
+                    }
+
+                    oDoc.Words.Last.InsertBreak(Microsoft.Office.Interop.Word.WdBreakType.wdPageBreak);
+                }
+
+                //oDoc.Save();
+
+                //oWord.Application.Quit(ref m, ref m, ref m);
+                txtOutput.Text = sbOutput.ToString();
+            }
+        }
+
+        public static string FormatXML(string XML)
+        {
+            string Result = "";
+
+            MemoryStream mStream = new MemoryStream();
+            XmlTextWriter writer = new XmlTextWriter(mStream, Encoding.Unicode);
+            XmlDocument document = new XmlDocument();
+
+            try
+            {
+                // Load the XmlDocument with the XML.
+                document.LoadXml(XML);
+
+                writer.Formatting = Formatting.Indented;
+
+                // Write the XML into a formatting XmlTextWriter
+                document.WriteContentTo(writer);
+                writer.Flush();
+                mStream.Flush();
+
+                // Have to rewind the MemoryStream in order to read
+                // its contents.
+                mStream.Position = 0;
+
+                // Read MemoryStream contents into a StreamReader.
+                StreamReader sReader = new StreamReader(mStream);
+
+                // Extract the text from the StreamReader.
+                String FormattedXML = sReader.ReadToEnd();
+
+                Result = FormattedXML;
+            }
+            catch (XmlException x)
+            {
+                MessageBox.Show(x.Message);
+            }
+
+            mStream.Close();
+            writer.Close();
+
+            return Result;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //txtLinks.Text = File.ReadAllText("links.txt");
+
+            string[] links = File.ReadAllLines("links.txt");
+
+            for (int i = 0; i < 5; i++)
+            {
+                txtLinks.Text += links[i] + Environment.NewLine;
+            }
+        }
+
+    }
+
+    public class HtmlToText
+    {
+        public HtmlToText()
+        {
+        }
+
+        public static string Convert(string path)
+        {
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc.Load(path);
+
+            StringWriter sw = new StringWriter();
+            ConvertTo(doc.DocumentNode, sw);
+            sw.Flush();
+            return sw.ToString();
+        }
+
+        public static string ConvertHtml(string html)
+        {
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(html);
+
+            StringWriter sw = new StringWriter();
+            ConvertTo(doc.DocumentNode, sw);
+            sw.Flush();
+            return sw.ToString();
+        }
+
+        private static void ConvertContentTo(HtmlAgilityPack.HtmlNode node, TextWriter outText)
+        {
+            foreach (HtmlAgilityPack.HtmlNode subnode in node.ChildNodes)
+            {
+                ConvertTo(subnode, outText);
+            }
+        }
+
+        public static void ConvertTo(HtmlAgilityPack.HtmlNode node, TextWriter outText)
+        {
+            string html;
+            switch (node.NodeType)
+            {
+                case HtmlAgilityPack.HtmlNodeType.Comment:
+                    // don't output comments
+                    break;
+
+                case HtmlAgilityPack.HtmlNodeType.Document:
+                    ConvertContentTo(node, outText);
+                    break;
+
+                case HtmlAgilityPack.HtmlNodeType.Text:
+                    // script and style must not be output
+                    string parentName = node.ParentNode.Name;
+                    if ((parentName == "script") || (parentName == "style"))
+                        break;
+
+                    // get text
+                    html = ((HtmlAgilityPack.HtmlTextNode)node).Text;
+
+                    // is it in fact a special closing node output as text?
+                    if (HtmlAgilityPack.HtmlNode.IsOverlappedClosingElement(html))
+                        break;
+
+                    // check the text is meaningful and not a bunch of whitespaces
+                    if (html.Trim().Length > 0)
+                    {
+                        outText.Write(HtmlAgilityPack.HtmlEntity.DeEntitize(html));
+                    }
+                    break;
+
+                case HtmlAgilityPack.HtmlNodeType.Element:
+                    switch (node.Name)
+                    {
+                        case "p":
+                            // treat paragraphs as crlf
+                            outText.Write("\r\n");
+                            break;
+                    }
+
+                    if (node.HasChildNodes)
+                    {
+                        ConvertContentTo(node, outText);
+                    }
+                    break;
+            }
+        }
+    }
+}
